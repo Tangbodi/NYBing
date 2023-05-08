74725b9090a978cb0af4b4a59755c2764b97a3a0
tangbodi <tangbodi@gmail.com>
2023-05-08 13:07:20 -0400

    5/8

--- demo/src/main/java/com/example/demo/WebApplication.java
+++ demo/src/main/java/com/example/demo/WebApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
 public class WebApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
