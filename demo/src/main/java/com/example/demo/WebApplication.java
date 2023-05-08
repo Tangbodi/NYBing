c92d8997331dac507931bb1fd732162f38a2d043
tangbodi <tangbodi@gmail.com>
2023-05-08 12:56:36 -0400

    5/8

--- demo/src/main/java/com/example/demo/WebApplication.java
+++ demo/src/main/java/com/example/demo/WebApplication.java
@@ -3,13 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class WebApplication {
-
+public class WebApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
 }
+
